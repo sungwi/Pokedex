6fcb1c093980244764f3a87ee784605a7785d795
Course Test <sungwi@cs.sfu.ca>
2024-10-30 04:06:57 -0700

    modify python version

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 # Update pip
 echo "Updating pip..."
-python3.9 -m pip install -U pip  # -m オプションを忘れないように
+python3.10 -m pip install -U pip  # -m オプションを忘れないように
 
 # Install dependencies
 echo "Installing project dependencies..."
-python3.9 -m pip install -r requirements.txt
+python3.10 -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
 
 # Collect staticfiles
 echo "Collect static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.10 manage.py collectstatic --noinput --clear
 
 echo "Build process completed!"
