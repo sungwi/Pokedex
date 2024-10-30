9760159196e52d3ab78a8666953a9bcfc4f366a6
Course Test <sungwi@cs.sfu.ca>
2024-10-30 04:30:45 -0700

    fix pip command

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Update pip
-echo "Updating pip..."
-python3.10 -m pip install -U pip
+# Install pip
+echo "Installing pip..."
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
 
 # Install dependencies
-echo "Installing dependencies..."
-python3.10 -m pip install -r requirements.txt
+echo "Installing project dependencies..."
+python3.9 -m pip install -r requirements.txt
 
-# Apply migrations
-echo "Applying migrations..."
-python3.10 manage.py migrate --noinput
+# Make migrations
+echo "Running migrations..."
+python3.9 manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-python3.10 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "Build process completed!"
