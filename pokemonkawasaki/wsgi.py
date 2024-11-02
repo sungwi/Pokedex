02aaacf7145fa32741d57a00c4e0166cfece4a7a
Course Test <sungwi@cs.sfu.ca>
2024-11-02 01:32:41 -0700

    fix wsgi

--- pokemonkawasaki/wsgi.py
+++ pokemonkawasaki/wsgi.py
@@ -1,18 +1,14 @@
-"""
-WSGI config for pokemonkawasaki project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemonkawasaki.settings')
 
 application = get_wsgi_application()
 
-app = application 
+# WhiteNoiseを利用して静的ファイルを提供する
+from django.conf import settings
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+
+# Vercel互換性のため
+app = application
