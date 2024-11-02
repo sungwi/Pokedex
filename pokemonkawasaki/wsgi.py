167da95e2ac56a23c8286fec130655755f4add6e
Course Test <sungwi@cs.sfu.ca>
2024-11-02 01:47:04 -0700

    modify

--- pokemonkawasaki/wsgi.py
+++ pokemonkawasaki/wsgi.py
@@ -1,18 +1,12 @@
-"""
-WSGI config for pokemonkawasaki project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemonkawasaki.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
 
 app = application 
