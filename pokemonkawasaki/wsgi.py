60b011a04a6a43838017824184d06b04aa4b46b7
Course Test <sungwi@cs.sfu.ca>
2024-11-06 01:13:00 -0800

    fix whitenoise

--- pokemonkawasaki/wsgi.py
+++ pokemonkawasaki/wsgi.py
@@ -6,7 +6,6 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemonkawasaki.settings')
 
 application = get_wsgi_application()
-
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
+application = WhiteNoise(application)
 
 app = application 
