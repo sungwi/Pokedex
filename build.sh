9136619a82116baf513ca295ac28d9bf946f0ef6
Course Test <sungwi@cs.sfu.ca>
2024-11-06 03:41:44 -0800

    temp .sh fix

--- build.sh
+++ build.sh
@@ -9,19 +9,19 @@ python3.9 get-pip.py
 echo "Installing project dependencies..."
 python3.9 -m pip install -r requirements.txt
 
-# Djangoのcontribアプリを一時的にコメントアウト
-sed -i '' 's/^    '\''django.contrib./#&/' pokemonkawasaki/settings.py
+# # Djangoのcontribアプリを一時的にコメントアウト
+# sed -i '' 's/^    '\''django.contrib./#&/' pokemonkawasaki/settings.py
 
-# Make migrations
-echo "Making migrations..."
-python3.9 manage.py makemigrations
+# # Make migrations
+# echo "Making migrations..."
+# python3.9 manage.py makemigrations
 
-# Apply migrations
-echo "Running migrations..."
-python3.9 manage.py migrate --noinput
+# # Apply migrations
+# echo "Running migrations..."
+# python3.9 manage.py migrate --noinput
 
-# コメントアウトを解除して元に戻す
-sed -i '' 's/^#    '\''django.contrib./    '\''django.contrib./' pokemonkawasaki/settings.py
+# # コメントアウトを解除して元に戻す
+# sed -i '' 's/^#    '\''django.contrib./    '\''django.contrib./' pokemonkawasaki/settings.py
 
 # Collect static files
 echo "Collecting static files..."
