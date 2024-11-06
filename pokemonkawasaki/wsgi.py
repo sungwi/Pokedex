43c5b2af5d5ad1ee679109706c083afd5867c0bb
Course Test <sungwi@cs.sfu.ca>
2024-11-06 03:55:13 -0800

    fix whitenoise

--- pokemonkawasaki/wsgi.py
+++ pokemonkawasaki/wsgi.py
@@ -1,11 +1,12 @@
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemonkawasaki.settings')
 
 application = get_wsgi_application()
+BASE_DIR = Path(__file__).resolve().parent.parent
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
 
 app = application 
