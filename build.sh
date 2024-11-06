8fad5c4887b1f510029046d12d7c706a359e55aa
Course Test <sungwi@cs.sfu.ca>
2024-11-06 03:34:49 -0800

    fix comment out path

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo "Installing project dependencies..."
 python3.9 -m pip install -r requirements.txt
 
 # Djangoのcontribアプリを一時的にコメントアウト
-sed -i '' 's/^    '\''django.contrib./#&/' path/to/your/settings.py
+sed -i '' 's/^    '\''django.contrib./#&/' pokemonkawasaki/settings.py
 
 # Make migrations
 echo "Making migrations..."
@@ -21,7 +21,7 @@ echo "Running migrations..."
 python3.9 manage.py migrate --noinput
 
 # コメントアウトを解除して元に戻す
-sed -i '' 's/^#    '\''django.contrib./    '\''django.contrib./' path/to/your/settings.py
+sed -i '' 's/^#    '\''django.contrib./    '\''django.contrib./' pokemonkawasaki/settings.py
 
 # Collect static files
 echo "Collecting static files..."
