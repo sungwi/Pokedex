ba2099229ec0f4474b5c3c800cd2f04f8cfdc795
Course Test <sungwi@cs.sfu.ca>
2024-11-06 02:47:13 -0800

    comment out cotrib

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ python3.9 get-pip.py
 echo "Installing project dependencies..."
 python3.9 -m pip install -r requirements.txt
 
+# Djangoのcontribアプリを一時的にコメントアウト
+sed -i '' 's/^    '\''django.contrib./#&/' path/to/your/settings.py
+
 # Make migrations
 echo "Making migrations..."
 python3.9 manage.py makemigrations
@@ -16,6 +19,9 @@ python3.9 manage.py makemigrations
 # Apply migrations
 echo "Running migrations..."
 python3.9 manage.py migrate --noinput
+
+# コメントアウトを解除して元に戻す
+sed -i '' 's/^#    '\''django.contrib./    '\''django.contrib./' path/to/your/settings.py
 
 # Collect static files
 echo "Collecting static files..."
